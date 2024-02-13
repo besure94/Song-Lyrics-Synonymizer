6175a9c002f4628da6d7fb26d9f38124d1de7eba
Jason Falk <jasonfalk9@gmail.com>
2024-02-13 14:30:48 -0800

    Add comments to make code more readable.

--- src/index.js
+++ src/index.js
@@ -5,18 +5,23 @@ import LyricsService from './js/LyricsService.js';
 import LyricsDumpStorage from './js/LyricsDumpStorage.js';
 import Synonymizer from './js/synonymizer.js';
 
+// this function awaits the getData() function from the LyricsService.js file
 async function getLyrics(title, artist) {
   let response = await LyricsService.getData(title, artist);
+  // stores the lyrics retrieved from the Lyrist API call in a variable, and returns the variable
   let returnLyrics = response.lyrics;
   return returnLyrics;
 }
 
+// this function awaits the synonymize() function from the Synonymizer.js file
 async function getSynonymizedLyrics(lyrics) {
   const synonymizedLyrics = await Synonymizer.synonymize(lyrics);
+  // stores the synonymized lyrics resulting from the Thesaurus and Compendium APIs in a variable, and returns the variable
   let returnSynonymizedLyrics = synonymizedLyrics;
   return returnSynonymizedLyrics;
 }
 
+// displays the song lyrics from the original song
 function displaySongLyrics(response) {
   document.querySelector("div#lyricsDiv").innerText = "";
   let displayLyricsDiv = document.createElement("div");
@@ -26,6 +31,7 @@ function displaySongLyrics(response) {
   document.querySelector("div#lyricsDiv").appendChild(displayLyricsDiv);
 }
 
+// displays the synonymized lyrics, aka the new lyrics
 function displaySynonymizedLyrics(synonymizedLyrics) {
   const displayLyricsDiv = document.createElement("div");
   const displayLyrics = document.createElement("p");
@@ -35,13 +41,13 @@ function displaySynonymizedLyrics(synonymizedLyrics) {
   document.querySelector("div#arrow").removeAttribute("class");
 }
 
-
-
-// TODO Add most tts functionality to sti own function.
+// accepts the synonymized lyrics as an argument, and converts these lyrics to speech
 function textToSpeech(lyricsToSpeak) {
+  // clears text to speech queue
   window.speechSynthesis.cancel();
   let lyrics = new SpeechSynthesisUtterance();
   let voices = window.speechSynthesis.getVoices();
+  // parameters for text to speech voice
   lyrics.voice = voices[2];
   lyrics.volume = 1;
   lyrics.rate = 1.5;
@@ -59,7 +65,9 @@ window.addEventListener("load", function (event) {
     let titleData = document.getElementById("song").value;
     let artistData = document.getElementById("artist").value;
 
+    // Calls getLyrics then waits for lyricsResponse return
     getLyrics(titleData, artistData).then(function (lyricsResponse) {
+      // stores lyricsResponse in lyricsStorage Object
       lyricsStorage.lyricsApiResponse = lyricsResponse;
       displaySongLyrics(lyricsStorage.lyricsApiResponse);
       const txtToSpeechControlDiv = document.getElementById("text-to-speech-control-buttons");
@@ -72,6 +80,7 @@ window.addEventListener("load", function (event) {
         evt.preventDefault();
         textToSpeech(lyricsStorage.lyricsApiResponse);
 
+        // text to speech pause and resume button - if pauseResumeButton value is 1, it switches to 2 and pauses, then resumes if button is pressed again
         const pauseResumeButton = document.getElementById("pause-resume-button");
         pauseResumeButton.addEventListener("click", function () {
           if (pauseResumeButton.value == 1 || pauseResumeButton.value == "") {
@@ -88,9 +97,11 @@ window.addEventListener("load", function (event) {
         const button = document.querySelector("#synonymize");
         button.setAttribute("disabled", "true");
         button.textContent = "Working...";
+        // calls getSynonymizedLyrics then waits for response
         getSynonymizedLyrics(lyricsStorage.lyricsApiResponse)
           .then(function (synonymizedLyricsResponse) {
             evt.preventDefault();
+            // stores synonymizedLyricsResponse in lyricsStorage
             lyricsStorage.synonymizedLyricsApiResponse = synonymizedLyricsResponse;
             displaySynonymizedLyrics(lyricsStorage.synonymizedLyricsApiResponse);
           })
@@ -100,7 +111,7 @@ window.addEventListener("load", function (event) {
           });
       });
       
-      // Prevents text to speech from continually play outside of website.
+      // Prevents text to speech from playing outside of website.
       window.addEventListener("beforeunload", function () {
         window.speechSynthesis.cancel();
       });
